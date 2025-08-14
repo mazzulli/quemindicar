a141b8c064132c80472da630cb72e88bc43e7e65
Mazzulli <mazzulli@live.com>
2025-08-13 23:28:57 -0300

    fix: fix context for payment confirmation

--- app/(checkout)/payment-confirmation/page.tsx
+++ app/(checkout)/payment-confirmation/page.tsx
@@ -5,8 +5,9 @@ const CheckoutReturnPage = () => {
     return ( 
         <div className="w-full max-w-md mx-auto flex flex-col items-center justify-center py-4 text-center">
             <h1 className="font-bold text-gray-700 mb-5">Pagamento realizado com sucesso!</h1>
-            <p>Obrigado por sua compra! Seu pagamento foi processado com sucesso.</p>
-            <p>Você receberá um e-mail de confirmação em breve.</p>
+            <p>Obrigado pela sua compra e por confiar em nossos serviços!</p>            
+            <p>Você receberá um e-mail de confirmação com instruções para realizar o seu cadastro</p>
+            <p>para divulgação do seu serviço ou negócio.</p>
             <Link href="/" className="text-blue-500 hover:underline mt-8">
                 Voltar para a página inicial
             </Link>
