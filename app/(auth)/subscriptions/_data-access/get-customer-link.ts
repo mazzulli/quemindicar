c4145e3ec0cfb4db0e39e73f8df4c9b26620f27c
Mazzulli <mazzulli@live.com>
2025-10-12 09:21:17 -0300

    fix: subscription page auth and redirect

--- app/(auth)/subscriptions/_data-access/get-customer-link.ts
+++ app/(auth)/subscriptions/_data-access/get-customer-link.ts
@@ -5,7 +5,7 @@ import { stripe } from "@/lib/stripe";
 const getCustomerLink = async (customerId: string) => {
   const session = await stripe.billingPortal.sessions.create({
     customer: customerId,
-    return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/users`,
+    return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`,
   });
   console.log("Customer Portal Session URL:", session.url);
   return session.url;
